06b075bca1f143b938d705e905e0fe1d02c52106
Carlos - portatil <crolindez@gmail.com>
2014-12-30 23:54:58 +0100

    forth change

--- Navigator/src/es/carlosrolindez/navigator/SettingsFragment.java
+++ Navigator/src/es/carlosrolindez/navigator/SettingsFragment.java
@@ -1,7 +1,9 @@
 package es.carlosrolindez.navigator;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
 
 public class SettingsFragment extends PreferenceFragment {
@@ -12,5 +14,45 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,String key) 
+    {
+      updateSummary();
+    }
+    
+    public void updateSummary()
+    {
+        EditTextPreference preference = (EditTextPreference) getPreferenceScreen().findPreference("server_name");
+        if (preference!=null) preference.setSummary(preference.getText());
+     
+        preference = (EditTextPreference) getPreferenceScreen().findPreference("ip_address");
+        preference.setSummary(preference.getText());
+        
+        preference = (EditTextPreference) getPreferenceScreen().findPreference("port_number");
+        preference.setSummary(preference.getText());
+        
+        preference = (EditTextPreference) getPreferenceScreen().findPreference("domain_name");
+        preference.setSummary(preference.getText());       
+        
+        preference = (EditTextPreference) getPreferenceScreen().findPreference("user_name");
+        preference.setSummary(preference.getText());       
+  
+    }
+
 
 }
